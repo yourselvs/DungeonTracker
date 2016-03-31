5948adf04379fcb291929f5e0a3c10d2eaefb85e
Michael <unconfigured@null.spigotmc.org>
2016-03-30 20:51:07 -0400

    Update AppTest.

--- src/test/java/me/yourselvs/AppTest.java
+++ src/test/java/me/yourselvs/AppTest.java
@@ -1,5 +1,7 @@
 package me.yourselvs;
 
+import java.util.ArrayList;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,9 +9,8 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
+	private Calc calc;
     /**
      * Create the test case
      *
@@ -27,12 +28,32 @@ public class AppTest
     {
         return new TestSuite( AppTest.class );
     }
-
+    
+    
+    protected void setUp() {
+     calc = new Calc();
+    }
+    
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testAddPositives()
     {
-        assertTrue( true );
+    	
+ 
+    	
+        assertTrue( calc.add(3,4) == 7 );
+    }
+    public void testAddNegatives()
+    {
+    
+    	assertTrue( calc.add(-3,  -4) == -7);
+    }
+    public void testDiv()
+    {
+    	assertTrue (calc.divide(15,5) == 3);
+    }
+    public void testDivByZero()
+    {
     }
 }
